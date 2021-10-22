dfc59122effc37fe5b22d6405e36372a3b293e1a
sergiocardenasg <sergiocardenas@utexas.edu>
2021-10-21 19:02:08 -0500

    Revert "solved"
    This reverts commit c7c8ccd59fc854dca193e553e8ffa4a970346af0.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -31,7 +31,6 @@ export default class YouTubeDebugger extends Component{
             settings: {
                 ...this.state.settings,
                 video: {
-                    ...this.state.settings.video,
                     resolution: '720p'
                 }
             }
